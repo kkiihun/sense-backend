671c76c227843df31809d4ba322a3e2bea6955f5
kkiihun <mydodream87@gmail.com>
2025-05-21 22:23:25 +0900

    models.py -update database.py -update

--- models.py
+++ models.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+# 기존 Sensedata 모델 그대로 유지
 class SenseData(Base):
     __tablename__ = "sensedata"
 
@@ -13,3 +14,11 @@ class SenseData(Base):
     keyword = Column(String, nullable=False)
     emotion_score = Column(Float, nullable=False)
     description = Column(String, nullable=True)
+
+# User model
+class User(Base):
+    __tablename__ = "users"
+    
+    id = Colum(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
